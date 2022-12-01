a13a2671d5b783e9cf844d2292264bc08cf1ec46
ZonRed <myansar70@gmail.com>
2022-12-01 08:57:22 +0700

    commit tes2

---
+++ src/OutputProgram.java
@@ -0,0 +1,9 @@
+public class OutputProgram {
+
+    public static void main(String[] args) {
+        String pbo="PBO";
+        int ankatan =2021;
+        System.out.printf("pratikum %s\n",pbo);
+        System.out.printf("Angkatan %s\n",ankatan);
+    }
+}
